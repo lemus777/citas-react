fcf8b0f86789ba2a5d50aa0210e96740b166f26c
lemus <lemus777@gmail.com>
2023-05-01 19:05:26 +0200

    formulario

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+const Paciente = () => {
+  return (
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-lg">
+      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: <span className="font-normal normal-case">Hook</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case">Jorge</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Email: <span className="font-normal normal-case">correo@correo.com</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: <span className="font-normal normal-case">10 Diciembre de 2022</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam praesentium nobis quod temporibus, eaque omnis labore facilis. Veniam unde laborum ut, praesentium voluptate perspiciatis voluptatum velit accusantium inventore sint.</span></p>
+    </div>
+  )
+}
+
+export default Paciente
