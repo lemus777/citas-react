1138f4d274b5b58ce25680ce5c0d1ea36bd68654
lemus <lemus777@gmail.com>
2023-05-04 14:00:29 +0200

    new useState for paciente

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes}) => {
+const ListadoPacientes = ({pacientes, setPaciente}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
       {pacientes && pacientes.length ? (
@@ -11,6 +11,7 @@ const ListadoPacientes = ({pacientes}) => {
             <Paciente
               key={paciente.id}
               paciente={paciente}
+              setPaciente={setPaciente}
             />
           ))}
         </>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,4 +1,4 @@
-const Paciente = ({paciente}) => {
+const Paciente = ({paciente, setPaciente}) => {
   const { nombre, propietario, email, fecha, sintomas } = paciente
 
   return (
@@ -12,6 +12,7 @@ const Paciente = ({paciente}) => {
         <button
           type='button'
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 transition-colors text-white font-bold uppercase rounded-lg"
+          onClick={() => setPaciente(paciente)}
         >Editar</button>
         <button
           type='button'
